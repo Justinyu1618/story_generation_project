ce284404114aabbf7d49cb2ccd863fff964ee19c
Justin Yu <justinyu1618@gmail.com>
2019-01-18 23:41:42 -0500

    added tensorflow implementation of CNN

--- train_conv_seq2seq.sh
+++ train_conv_seq2seq.sh
@@ -1,25 +1,6 @@
-# Convolutional Seq2Seq
-
-This is a tensorflow implementation of the [convolutional seq2seq model](https://arxiv.org/abs/1705.03122) released by Facebook. This model is orignially written via Torch/Lua in [Fairseq](https://github.com/facebookresearch/fairseq). Considering Lua is not that popular as python in the industry and research community, I re-implemente this model with Tensorflow/Python after carefully reading the paper details and Torch/Lua codebase.     
-
-This implementation is based on the framework of [Google seq2seq project](https://github.com/google/seq2seq), which has a detailed [documentation](https://google.github.io/seq2seq/) on how to use this framework. In this conv seq2seq project, I implement the conv encoder, conv decoder, and attention mechanism, as well as other modules needed by the conv seq2seq model, which is not available in the original seq2seq project. 
-
-
-## Requirement
-
-- Python 2.7.0+
-- [Tensorflow](https://github.com/tensorflow/tensorflow) 1.0+ (this version is strictly required)
-- and their dependencies
-
-Please follow [seq2seq project](https://google.github.io/seq2seq/) on how to install the Convolutional Sequence to Sequence Learning project. 
-## How to use
-For dataset, please follow [seq2seq nmt guides](https://google.github.io/seq2seq/nmt/) to prepare your dataset
-
-The following is an example of how to run iwslt de-en translation task.
-### Train
-```
 export PYTHONIOENCODING=UTF-8
-export DATA_PATH="your iwslt de-en data path"
+
+export DATA_PATH=/home/xutan/nmt/seq2seq/data/iwslt14.tokenized.de-en
 
 export VOCAB_SOURCE=${DATA_PATH}/vocab.de
 export VOCAB_TARGET=${DATA_PATH}/vocab.en
@@ -32,9 +13,11 @@ export TEST_TARGETS=${DATA_PATH}/test.en
 
 export TRAIN_STEPS=1000000
 
+
 export MODEL_DIR=${TMPDIR:-/tmp}/nmt_conv_seq2seq
 mkdir -p $MODEL_DIR
 
+'''
 python -m bin.train \
   --config_paths="
       ./example_configs/conv_seq2seq.yml,
@@ -62,17 +45,13 @@ python -m bin.train \
   --train_steps $TRAIN_STEPS \
   --output_dir $MODEL_DIR
 
-```
+'''
 
-### Test
 
-```
 export PRED_DIR=${MODEL_DIR}/pred
 mkdir -p ${PRED_DIR}
-```
-
-#### decode with greedy search
-```
+'''
+###with greedy search
 python -m bin.infer \
   --tasks "
     - class: DecodeText" \
@@ -87,10 +66,8 @@ python -m bin.infer \
         - $TEST_SOURCES" \
   > ${PRED_DIR}/predictions.txt
 
-```
-
-#### decode with beam search
-```
+'''
+###with beam search
 python -m bin.infer \
   --tasks "
     - class: DecodeText
@@ -107,17 +84,9 @@ python -m bin.infer \
       source_files:
         - $TEST_SOURCES" \
   > ${PRED_DIR}/predictions.txt
-```
 
-#### calculate BLEU score
-```
+
 ./bin/tools/multi-bleu.perl ${TEST_TARGETS} < ${PRED_DIR}/predictions.txt
-```
 
-
-For more detailed instructions, please refer to [seq2seq project](https://google.github.io/seq2seq/).
-
-
-Issues and contributions are warmly welcome.  
 
 
